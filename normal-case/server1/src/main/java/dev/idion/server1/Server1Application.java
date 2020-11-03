50f2d1a7e78280a801c0b36e073dce8f9f89154f
Dion <ksundong@gmail.com>
2020-11-03 21:18:06 +0900

    feat: 한 API서버에서 다른 API서버의 기능을 호출해서 사용하는 API구현

--- normal-case/server1/src/main/java/dev/idion/server1/Server1Application.java
+++ normal-case/server1/src/main/java/dev/idion/server1/Server1Application.java
@@ -2,9 +2,17 @@ package dev.idion.server1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Server1Application {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Server1Application.class, args);
--- normal-case/server2/src/main/java/dev/idion/server2/Server2Application.java
+++ normal-case/server2/src/main/java/dev/idion/server2/Server2Application.java
@@ -1,10 +1,24 @@
 package dev.idion.server2;
 
+import java.net.URI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class Server2Application {
+
+	@GetMapping("/hi")
+	public String hi() {
+		final RestTemplate restTemplate = new RestTemplate();
+		final ResponseEntity<String> entity = restTemplate
+				.getForEntity(URI.create("http://localhost:8080/hello"), String.class);
+		return "server1: " + entity.getBody();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Server2Application.class, args);
